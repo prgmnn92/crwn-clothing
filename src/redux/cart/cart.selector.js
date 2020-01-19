1883bea0db112ad6168a56568bbc78f6226fe743
Phillip Pargmann <pargmann92@gmail.com>
2020-01-19 09:25:00 +0100

    added checkout page and toggle dropdown by going on checkout page

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -9,6 +9,10 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
@@ -17,4 +21,12 @@ export const selectCartItemsCount = createSelector(
         accumalatedQuanitity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], cartItems =>
+  cartItems.reduce(
+    (accumalatedQuanitity, cartItem) =>
+      accumalatedQuanitity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
